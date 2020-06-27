a39ff18b71ba9e8d6c5bd19b6d904b93ceba701c
GiohPergentino <67512234+GiohPergentino@users.noreply.github.com>
2020-06-27 12:16:25 -0300

    Primeira vez

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { ButtonComponent } from './button/button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    ButtonComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
